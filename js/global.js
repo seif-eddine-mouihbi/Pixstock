dff330a15acd0a4a538a6c4c8619e781b10d0a7e
seif-eddine-mouihbi <mouihbiseif@gmail.com>
2024-07-15 17:53:44 +0100

    update level 1

--- js/global.js
+++ js/global.js
@@ -4,3 +4,28 @@
  */
 
 "use strict";
+
+/**
+ * Import
+ */
+
+import { ripple } from "./ripple.js";
+
+/**
+ * Header on-scroll state
+ */
+
+const /** {NodeElement} */ $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+  $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+
+/**
+ * Add ripple effect
+ */
+
+const /** {NodeElement} */ $rippleElems = document.querySelectorAll('[data-ripple]')
+
+$rippleElems.forEach($rippleElem => ripple($rippleElem))
---
+++ js/ripple.js
@@ -0,0 +1,52 @@
+/**
+ * @copyright Seif Eddine Mouihbi 2024
+ * @author Seif <mouihbiseif@gmail.com>
+ */
+
+"use strict";
+
+/**
+ * Add ripple effect on an element
+ * @param {Node} $rippleElem Element for ripple effect
+ */
+
+export const ripple = function ($rippleElem) {
+  $rippleElem.addEventListener("pointerdown", function (e) {
+    e.stopImmediatePropagation();
+
+    const /** {NodeElement} */ $ripple = document.createElement("div");
+    $ripple.classList.add("ripple");
+    this.appendChild($ripple); // this : the parent element 'Button'
+
+    const removeRipple = () => {
+      $ripple.animate(
+        {
+          opacity: 0,
+        },
+        { fill: "forwards", duration: 200 }
+      );
+
+      setTimeout(() => {
+        $ripple.remove();
+      }, 1000);
+    };
+
+    this.addEventListener("pointerup", removeRipple);
+    this.addEventListener("pointerleave", removeRipple);
+
+    const /** {Boolean} */ isNotIconButton =
+        !this.classList.contains("icon-btn");
+
+    if (isNotIconButton) {
+      const /** {Number} */ rippleSize = Math.max(
+          this.clientWidth,
+          this.clientHeight
+        );
+
+      $ripple.style.top = `${e.layerY}px`;
+      $ripple.style.left = `${e.layerX}px`;
+      $ripple.style.width = `${rippleSize}px`;
+      $ripple.style.height = `${rippleSize}px`;
+    }
+  });
+};
